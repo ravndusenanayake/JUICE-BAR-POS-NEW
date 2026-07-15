1aac28489562232e6f03dd5bad3d231ce07f6ff9
ravindu senanayake <123373439+ravndusenanayake@users.noreply.github.com>
2026-07-15 10:30:26 +0530

    chore: add utility scripts to purge invalid GRN records and test entries from database

---
+++ scratch-delete-0.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://ravindusenanayake3415_db_user:wXl9SaRrJyPwusen@cluster0.iazs7qg.mongodb.net/juice-bar-pos?retryWrites=true&w=majority&appName=Cluster0').then(async () => {
+  const db = mongoose.connection.db;
+  const res = await db.collection('grns').deleteMany({
+    $or: [
+      { totalAmount: 0 },
+      { totalAmount: null },
+      { totalAmount: { $exists: false } }
+    ]
+  });
+  console.log('Deleted:', res.deletedCount);
+  mongoose.disconnect();
+}).catch(console.error);
